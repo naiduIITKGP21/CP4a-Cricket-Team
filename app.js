5db988694bf37678ac1cdf4deef0522ed5ce4ee2
ammkutti2310 <naiduiitkgp21@gmail.com>
2022-03-23 07:56:00 +0000

    error while running please debug error

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require("path"); //importing core module path
 //connecting SQLite database
 const dbPath = path.join(__dirname, "cricketTeam.db"); //path of database
 const sqlite3 = require("sqlite3"); // importing sqlite3 database
+app.use(express.json());
 
 let db = null;
 const initializeDBAndServer = async () => {
@@ -20,16 +21,28 @@ const initializeDBAndServer = async () => {
         console.log("server Running at http://localhost:3000/");
       };
   } catch (e) {
-    console.log("DB Error:${e.message}");
+    console.log("DB Error: ${e.message}");
     process.exit(1);
   }
 };
 
 initializeDBAndServer();
+ const convertJsonObjectToResponseObject = (dbObject)=>{
+return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number ,
+    role: role
+  };
+ };
 
 //API 1 : Returns a list of all players in the team
 app.get("/players/", async (request, resolve) => {
   const getAllPlayersOfTeam = `SELECT * FROM cricket_team;
     `;
-  const playerArray = await db.all(getAllPlayersOfTeam);
+  const playersArray = await db.all(getAllPlayersOfTeam);
+  console.log(response);
+  response.send(
+      playersArray.map((eachPlayer)=>convertJsonObjectToResponseObject(eachPlayer);)
+  )
 });
