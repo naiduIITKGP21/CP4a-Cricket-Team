8aba3fde37e525841a0e81d738483415b1523195
ammkutti2310 <naiduiitkgp21@gmail.com>
2022-03-23 07:06:24 +0000

    Handling connection exceptions

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ const path = require("path"); //importing core module path
 //connecting SQLite database
 const dbPath = path.join(__dirname, "cricketTeam.db"); //path of database
 const sqlite3 = require("sqlite3"); // importing sqlite3 database
+let db = null;
 const initializeDBAndServer = async () => {
-  await open({
-    filename: dbPath,
-    driver: sqlite3.Database,
-  });
-  app.listen(3000),
-    () => {
-      console.log("server Running at http://localhost:3000/");
-    };
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000),
+      () => {
+        console.log("server Running at http://localhost:3000/");
+      };
+  } catch (e) {
+    console.log("DB Error:${e.message}");
+    process.exit(1);
+  }
 };
+
 initializeDBAndServer();
