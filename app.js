ae2f1aa7ba5d79f67eba145d06cbc3c1955f405f
ammkutti2310 <naiduiitkgp21@gmail.com>
2022-03-23 06:58:07 +0000

    connecting SQLite Data base

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+//starting the express JS server
 const express = require("express");
 const app = express();
-app.listen(3000),
-  () => {
-    console.log("server Running at http://localhost:3000/");
-  };
+const { open } = require("sqlite"); //Importing from sqlite package
+const path = require("path"); //importing core module path
+
+//connecting SQLite database
+const dbPath = path.join(__dirname, "cricketTeam.db"); //path of database
+const sqlite3 = require("sqlite3"); // importing sqlite3 database
+const initializeDBAndServer = async () => {
+  await open({
+    filename: dbPath,
+    driver: sqlite3.Database,
+  });
+  app.listen(3000),
+    () => {
+      console.log("server Running at http://localhost:3000/");
+    };
+};
+initializeDBAndServer();
